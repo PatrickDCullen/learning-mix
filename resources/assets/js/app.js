5f4b9e78360eb0c3f66eb01d2333ea3f56cd32f1
PatrickDCullen <cullenpatrickd@gmail.com>
2021-02-24 15:33:35 -0500

    End Ep 9: Async/Await Support Out of the Box

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,15 @@
-class Person {
-    foo = "bar";
-    static baz = "baz";
+// const { default: axios } = require("axios");
+
+import axios from "axios";
+
+function getPosts() {
+    return axios.get("/posts");
 }
+
+async function fire() {
+    let response = await getPosts();
+
+    console.log(response.data[1]);
+}
+
+fire();
