a29a6479b1ef51471766ae34c12a6a7ca3738c55
PatrickDCullen <cullenpatrickd@gmail.com>
2021-02-24 15:54:29 -0500

    End Ep 10: Mix Extension Essentials

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require("laravel-mix");
 
+mix.extend("foobar", new MyExtension());
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
